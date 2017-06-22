2212fdd4621e0e0d5968cf7aabad21351dee1ee0
Richard van der Hoff <richard@matrix.org>
2017-06-22 12:01:30 +0100

    decode.py: Print symbol names

--- decode.py
+++ decode.py
@@ -11,10 +11,15 @@
 
 from __future__ import print_function
 
+import codecs
 import json
 import sys
 
 import python_sourcemap
+
+# tell python it can write utf8 to stdout (even if it's piped somewhere else)
+UTF8Writer = codecs.getwriter('utf8')
+sys.stdout = UTF8Writer(sys.stdout)
 
 srcmap = json.load(sys.stdin)
 line_table = python_sourcemap.decode_mappings(srcmap['mappings'])
@@ -23,6 +28,14 @@ for ln, line in enumerate(line_table):
     for cn in sorted(line.keys()):
         seg = line[cn]
         (src_id, src_line, src_col, name_id) = seg
-        source = srcmap['sources'][src_id]
-        print ("%i:%i: '%s':%i:%i (%i)" %
-               (ln+1, cn, source, src_line+1, src_col, name_id))
+        source = '<>'
+        if src_id is not None:
+            source = srcmap['sources'][src_id]
+        name = '<>'
+        if name_id is not None:
+            name = srcmap['names'][name_id]
+        print (
+            "%i:%i: '%s':%i:%i (%s)" % (
+                ln+1, cn, source, src_line+1, src_col, name,
+            )
+        )
